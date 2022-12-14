7378d626a9ce172f29549ac0dbbedf06ef80117d
Eryoneta <eryoneta@gmail.com>
2022-12-13 21:26:53 -0300

    (TEST) Injeção de frames
    - Teste de injeção de html em <iframe>

--- LocalHub/LHFactory/LHCore/lhcore.js
+++ LocalHub/LHFactory/LHCore/lhcore.js
@@ -1,16 +1,24 @@
 class LHCore {
-    //VARS
+    //GLOBAL VARS
     static VARIABLE_TAG = "lh-var";
+    static HUB_TAG = "lh-hub";
+    //ROUTES
+    static routes = {};
+    static createRoutes(routes = [{ url: "", path: "" }], window = undefined) {
+        this.routes[window] = routes;
+    }
+    static getRoute(url = "", window = undefined) {
+        const route = LHCore.routes[window].find(route => route.url === url);
+        if (!route) return undefined;
+        return route.path;
+    }
     //MAIN
     constructor() { }
-    //FUNCS
-    static createRoutes(routes = [{ url: "", path: "" }]) {
-        console.log(routes);
-    }
+    //BINDERS
     static getById(id = "") {
         return document.getElementById(id);
     }
-    static bindVariable(prop) {
+    static bindVariable(prop = "") {
         const elems = [...document.getElementsByTagName(LHCore.VARIABLE_TAG)].filter((elem) => elem.hasAttribute(prop));
         return {
             nativeElements: elems,
@@ -22,16 +30,49 @@ class LHCore {
             }
         };
     }
-    static bindView(prop) {
-        const elems = [...document.getElementsByTagName("iframe")].filter((elem) => elem.hasAttribute(prop));
+    static bindHub(prop = "") {
+        const elems = [...document.getElementsByTagName(LHCore.HUB_TAG)].filter((elem) => elem.hasAttribute(prop));
         return {
             nativeElements: elems,
             get viewPath() {
-                return this.nativeElements[0].href;
+                return this.nativeElements[0].innerHTML;
             },
             set viewPath(value) {
-                for (let element of this.nativeElements) element.href = value;
+                for (let element of this.nativeElements) element.innerHTML = value;
             }
         };
+    }
+    //LOADERS
+    static loadMainHub(window = undefined) {
+        window.addEventListener("message", (data) => {
+            switch (data.request) {
+                case "changeTitle":
+                    LHCore.getById("title").innerHTML = data.value;
+                    break;
+                case "changeIcon":
+                    LHCore.getById("icon").href = data.value;
+                    break;
+            }
+        });
+        window.addEventListener("hashchange", () => this._reloadPage(window));
+        this._reloadPage(window);
+    }
+    static loadHub(window = undefined) {
+        window.addEventListener("hashchange", () => this._reloadPage(window));
+        this._reloadPage(window);
+    }
+    static _reloadPage(window = undefined) {
+        const urlAtual = window.location.hash.replace("#", "");
+        const hubPath = LHCore.getRoute(urlAtual, window);
+        const hubs = LHCore.bindHub("routed");
+        for (let element of hubs.nativeElements) {
+            const content = document.createElement("iframe");
+            content.src = hubPath;
+            const corePath = "./LHFactory/LHCore/lhcore.js";
+            content.onload = () => {
+                content.contentWindow.postMessage({ corePath: corePath }, "*");
+            };
+            element.replaceChildren(content);
+        }
     }
 }
